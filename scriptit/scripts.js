a1dfd875d6ab21aca37e2508a71a7fa3c8eb28ae
Swaggypanties <kausejere@gmail.com>
2024-05-14 17:55:26 +0300

    commit

---
+++ scriptit/scripts.js
@@ -0,0 +1,17 @@
+$('#button').click(function(){
+    $('h1').hide();
+    $('h1').fadeIn(1800);
+});
+$('#button2').click(function(){
+    $('h1').fadeOut(1800);
+});
+$('#button3').click(function(){
+    $('h1').slideUp(1800);
+});
+$('#button4').click(function(){
+    $('h1').slideDown(1800);
+});
+$('#button5').click(function(){
+    $('#button5').animate({width: "300px", fontsize: "150%"}, 2800);
+    $('#button5').animate({width: "150px", fontsize: "50%"},3800);
+});
