8e5f3abb3a8b449bea3ce8c1647c4c345aec5552
Swaggypanties <kausejere@gmail.com>
2024-05-14 23:31:00 +0300

    Good progress

--- scriptit/scripts.js
+++ scriptit/scripts.js
@@ -1,17 +1,10 @@
-$('#button').click(function(){
-    $('h1').hide();
-    $('h1').fadeIn(1800);
-});
-$('#button2').click(function(){
-    $('budy').fadeOut(1800);
-});
-$('#button3').click(function(){
-    $('h1').slideUp(1800);
-});
-$('#button4').click(function(){
-    $('h1').slideDown(1800);
-});
-$('#button5').click(function(){
-    $('#button5').animate({width: "300px", fontsize: "150%"}, 2800);
-    $('#button5').animate({width: "150px", fontsize: "50%"},3800);
-});
+$(document).ready(function(){
+    $("#Button2").click(function(){
+      $("#box2").fadeOut();
+      $("#bye").fadeIn(3000);
+      var audio = document.getElementById('audio');
+        audio.play();
+    });
+  });
+
+  
