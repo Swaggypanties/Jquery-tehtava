643783af9878b64a150485e043fee3d0ef5b6491
Swaggypanties <kausejere@gmail.com>
2024-05-16 22:52:24 +0300

    Adding some cool stuff

--- scriptit/scripts.js
+++ scriptit/scripts.js
@@ -7,4 +7,38 @@ $(document).ready(function(){
     });
   });
 
-  
+$(document).ready(function() {
+    $("#Button1").click(function() {
+        $("#Budy").fadeOut(500, function() {
+            window.location.href = "menu.html";
+          });
+        });
+    });
+$(document).ready(function() {
+    $("#MenuButton1").click(function() {
+        $("#MenuBudy").fadeOut(500, function() {
+            window.location.href = "LureSelector.html";
+          });
+        });
+    });
+$(document).ready(function() {
+    $("#MenuButton2").click(function() {
+        $("#MenuBudy").fadeOut(500, function() {
+            window.location.href = "LureLibrary.html";
+          });
+        });
+    });
+$(document).ready(function() {
+    $("#MenuButton3").click(function() {
+        $("#MenuBudy").fadeOut(500, function() {
+            window.location.href = "Weather.html";
+          });
+        });
+    });
+$(document).ready(function() {
+    $("#MenuButton4").click(function() {
+        $("#MenuBudy").fadeOut(500, function() {
+            window.location.href = "Farewell.html";
+          });
+        });
+    });
