259d6ae3a049d250e80a6c202a2caf23d4862af4
Swaggypanties <kausejere@gmail.com>
2024-05-23 13:40:04 +0300

    Adding still stuff

--- scriptit/scriptsLure.js
+++ scriptit/scriptsLure.js
@@ -7,18 +7,18 @@ $(document).ready(function() {
     });
     //Lisää vaihtoehtoi valikkoon
     $(document).ready(function() {
-        // Populate the "Sunny" select element
+        // Sunny or cloudy
         $("#Sunny").append('<option value="Sunny">Sunny</option>');
         $("#Sunny").append('<option value="Cloudy">Cloudy</option>');
 
-        // Populate the "Rain" select element
+        // Raining or not
         $("#Rain").append('<option value="Yes">Yes</option>');
         $("#Rain").append('<option value="No">No</option>');
 
-        // Populate the "Temp" select element
-        $("#Temp").append('<option value="Cold">Cold 5...(-5)</option>');
-        $("#Temp").append('<option value="Warm">Warm 6...15</option>');
-        $("#Temp").append('<option value="Hot">Hot 16...25</option>');
+        // Temperature
+        $("#Temp").append('<option value="Cold">Cold 5°C...(-5°C)</option>');
+        $("#Temp").append('<option value="Warm">Warm 6°C...15°C</option>');
+        $("#Temp").append('<option value="Hot">Hot 16°C...25°C</option>');
 
 
         //Water clarity
